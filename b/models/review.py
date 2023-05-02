ad07a4ddcbd560b03129c16cec91e94b99f90055
Ademez <ademolaoladosu1@gmail.com>
2023-05-02 08:31:06 -0700

    All Task

---
+++ b/models/review.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+""" holds class Review"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+
+
+class Review(BaseModel, Base):
+    """Representation of Review """
+    if models.storage_t == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+
